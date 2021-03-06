3b19d51f0bfa23c2586769fccb24ff76f8a9e3b6
bill0925 <80153594+bill0925@users.noreply.github.com>
2021-03-06 20:39:02 +0900

    Update 이손혁.py

--- 백준/이손혁.py
+++ 백준/이손혁.py
@@ -1,12 +1,10 @@
 n = int(input())
 
 def fibo(n):
-    if n == 1:
-        return 1
-    elif n == 2:
-        return 1
-    elif n == 0:
+    if n == 0:
         return 0
+    elif n == 1:
+        return 1
     else:
         return fibo(n-1) + fibo(n-2)
 
