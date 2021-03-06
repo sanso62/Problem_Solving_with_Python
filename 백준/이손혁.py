45fd64a09c3baa4a468ed5c06b8325ff7d6f1974
bill0925 <80153594+bill0925@users.noreply.github.com>
2021-03-06 20:36:44 +0900

    Create 이손혁.py

---
+++ 백준/이손혁.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+def fibo(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 1
+    elif n == 0:
+        return 0
+    else:
+        return fibo(n-1) + fibo(n-2)
+
+
+print(fibo(n))
