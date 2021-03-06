216eca588103e1805dfd4372dbdf5ef14c6b7da0
minho <59383800+pot1223@users.noreply.github.com>
2021-03-06 21:00:12 +0900

    Create 송민호.py

---
+++ 백준/송민호.py
@@ -0,0 +1,12 @@
+numbers = []
+new_numbers = []
+n = int(input())
+if n == 1 :
+  print(n)
+elif n == 2 :
+  print(n)
+else :
+  for i in range(n)
+      numbers[i] = i
+      new_numbers[] = numbers[]
+      print(new_numbers[n-1] + new_numbers[n-2])      
