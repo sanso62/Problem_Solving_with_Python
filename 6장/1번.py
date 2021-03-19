7ba35a4b7c8ed439ec8b735f7f3115da3f685fe0
minho <59383800+pot1223@users.noreply.github.com>
2021-03-19 23:42:24 +0900

    Create 1번.py

---
+++ 6장/1번.py
@@ -0,0 +1,15 @@
+import Deque
+import collections
+
+def isPalindrome(self, s:str) :
+    
+    strs : Deque = collections.deque()
+    
+    for char in s:
+        if char.isalnum():
+            strs.append(char.lower())
+            
+        while len(strs) > 1 :
+            if strs.pop(0) != strs.pop():
+                return False
+    return True
