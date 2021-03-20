7ede4625cf4ff04cd8692a56c0e85c873726adde
minho <59383800+pot1223@users.noreply.github.com>
2021-03-20 19:24:22 +0900

    Update 01.py

--- 6장/송민호/01.py
+++ 6장/송민호/01.py
@@ -9,7 +9,7 @@ def isPalindrome(self, s:str) :
         if char.isalnum():
             strs.append(char.lower())
             
-        while len(strs) > 1 :
-            if strs.pop(0) != strs.pop():
-                return False
+    while len(strs) > 1 :
+         if strs.pop(0) != strs.pop():
+            return False
     return True
