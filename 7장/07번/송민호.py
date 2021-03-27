b8ac0ae9a9f3a1e910f6d9b96208a7e0cccdf78f
minho <59383800+pot1223@users.noreply.github.com>
2021-03-27 19:22:08 +0900

    Update 송민호.py

--- 7장/07번/송민호.py
+++ 7장/07번/송민호.py
@@ -1,5 +1,5 @@
 def numplus (self, nums:List[int],target :int) :
-    for i in (len(nums)):
-        for j in (i+1,len(nums)):
+    for i in range(len(nums)):
+        for j in range(i+1,len(nums)):
             if nums[i]+nums[j] == target:
                 return[i,j]
