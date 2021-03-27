78dad3b9055d5ea941e4590a86ab41202f31417e
minho <59383800+pot1223@users.noreply.github.com>
2021-03-27 21:14:50 +0900

    Create 송민호.py

---
+++ 백준/8958/송민호.py
@@ -0,0 +1,16 @@
+def score(self,result:str):
+    final_score = 0
+    scores=0
+    for i in result:
+        if i == 'O':
+            scores += 1
+        elif i == 'X':
+            final_score = scores
+            scores = 0 
+        else:
+            pass
+        
+   if 'X' in result :
+       return final_score
+   else:
+       return scores
