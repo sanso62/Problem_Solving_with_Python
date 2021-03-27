3f25a733954abdb0106a7d8417a1d762ce0ca290
bill0925 <80153594+bill0925@users.noreply.github.com>
2021-03-27 19:45:38 +0900

    Update 이손혁.py

--- 7장/09번/이손혁.py
+++ 7장/09번/이손혁.py
@@ -1,13 +1,13 @@
 nums = [-1, 0, 1, 2, -1, -4]
 
 for i in range(len(nums)):
-    for j in range(len(nums)):
+    for j in range(i+1,len(nums)):
         if j == i:
             False
-        for x in range(len(nums)):
+        for x in range(j+1,len(nums)):
             if x == j:
                 False
             if nums[i] + nums[j] + nums[x] == 0:
                 print(nums[i],nums[j],nums[x])
                
-# 실패함 중복 되는것 제외 실패
+# 실패함 중복 되는것 제외 실패.
