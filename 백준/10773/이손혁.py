72c3494428a87a0b76700c370bba0392cf7420db
Min dong Jo <65884076+sanso62@users.noreply.github.com>
2021-05-01 20:26:52 +0900

    Update 이손혁.py

--- 백준/10773/이손혁.py
+++ 백준/10773/이손혁.py
@@ -2,9 +2,9 @@ count = int(input())
 stack = []
 
 for i in range(count):
-num = int(input())
-if(num == 0):
-stack.pop()
-else:
-stack.append(num)
+  num = int(input())
+  if(num == 0):
+    stack.pop()
+  else:
+    stack.append(num)
 print(sum(stack))
