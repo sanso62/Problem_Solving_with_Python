1ac396764cae1820289fe4c7fb944107735307a8
starboard98 <55227905+starboard98@users.noreply.github.com>
2021-05-08 12:13:02 +0900

    Create 박우현.py

---
+++ 백준/1966/박우현.py
@@ -0,0 +1,32 @@
+TestCase=int(input())
+
+for t in range(TestCase):
+    string=input()
+    N=int(string[0])
+    M=int(string[2])
+    importance=list()
+    queue=[]
+    for i in range(9):
+        importance.append([])
+
+    string=input()
+    for i in range(N):
+        importance[int(string[i*2])-1].append(i+1)
+
+    last=0
+    ll=int()
+    for i in range(8,-1,-1):
+        for j in range(len(importance[i])):
+            if last<importance[i][j]:
+                queue.append(importance[i][j])
+                ll=importance[i][j]
+        for j in range(len(importance[i])):
+            if last>importance[i][j]:
+                queue.append(importance[i][j])
+                ll=importance[i][j]
+        last=ll
+
+    for i in range(len(queue)):
+        if queue[i]==M+1:
+            print(i+1)
+            break
