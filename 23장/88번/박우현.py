aa8aa2ec7f556752781446a00caf0b9938ccd226
starboard98 <55227905+starboard98@users.noreply.github.com>
2021-05-16 06:09:36 +0900

    Update 박우현.py

--- 23장/88번/박우현.py
+++ 23장/88번/박우현.py
@@ -1,13 +1,10 @@
 class Solution:
 	def rob(self, nums: List[int]) -> int:
-		used, notUsed=[0]*len(nums), [0]*len(nums)
+		used=[0]*len(nums)
 
 		for i in range(len(nums)):
 			for j in range(i-1):
 				used[i]=max(used[i],used[j])
-			for j in range(i):
-				used[i]=max(used[i],notUsed[j])
-				notUsed[i]=max(used[i],used[i-1])
 			used[i]+=nums[i]
 
-		return max(used[len(nums)-1],notUsed[len(nums)-1])
+		return max(used[len(nums)-1],used[len(nums)-2])
